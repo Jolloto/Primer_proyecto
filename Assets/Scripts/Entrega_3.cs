37c869becdfcfc10ae2e4d7e1cd7737f474f2e20
Jolloto <jolloto02@gmail.com>
2023-11-06 09:45:23 +0100

    Devuelta_el3

--- Assets/Scripts/Entrega_3.cs
+++ Assets/Scripts/Entrega_3.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Entrega_3 : MonoBehaviour
 {
    [SerializeField] private float num1;
    [SerializeField] private float num2;
+    /*
+       [SerializeField] private bool a = false;
+       [SerializeField] private bool e = false;
+       [SerializeField] private bool i = false;
+       [SerializeField] private bool o = false;
+       [SerializeField] private bool u = false;
+    */
 
+<<<<<<< Updated upstream
    
 
    [SerializeField] private bool a = false;
@@ -16,6 +26,24 @@ public class Entrega_3 : MonoBehaviour
    [SerializeField] private bool u = false;
 
    [SerializeField] private int grade = 9;
+=======
+    private string letra = "j";
+
+    [SerializeField] private int day = 31;
+    [SerializeField] private int month = 12;
+    [SerializeField] private int year = 2023;
+
+   [SerializeField] private float num3;
+   [SerializeField] private float num4;
+
+   [SerializeField] private int age = 65;
+
+   [SerializeField] private string range;
+
+   [SerializeField] private int num3;
+   [SerializeField] private int num4;
+
+>>>>>>> Stashed changes
 
    [SerializeField] private int mynumber;
    [SerializeField] private int mynumber2;
@@ -34,23 +62,23 @@ public class Entrega_3 : MonoBehaviour
           Debug.Log($"{num2} es mas grande");
        }
 
-       if (a) 
+       if (letra == "a") 
        {
             Debug.Log("Es vocal");
        }
-       else if (e)
+       else if (letra == "e")
        {
             Debug.Log("Es vocal");
        }
-       else if (i)
+       else if (letra == "i")
        {
             Debug.Log("Es vocal");
        }
-       else if (o)
+       else if (letra == "o")
        {
             Debug.Log("Es vocal");
        }
-       else if (u)
+       else if (letra == "u")
        {
             Debug.Log("Es vocal");
        }
@@ -59,6 +87,7 @@ public class Entrega_3 : MonoBehaviour
             Debug.Log("Es consonante");
        }
 
+<<<<<<< Updated upstream
        if (grade >= 9) 
       {
          Debug.Log("A");
@@ -94,7 +123,140 @@ public class Entrega_3 : MonoBehaviour
 
      
   
+=======
+        if (age > 65)
+        {
+            if (range == "gold")
+            {
+                Debug.Log("75% de descuento");
+            }
+            if (range == "silver")
+            {
+                Debug.Log("50% de descuento");
+            }
+            if (range == "bronze")
+            {
+                Debug.Log("25% de descuento");
+            }
+        }
+        else if (age < 65)
+        {
+            if (range == "gold")
+            {
+                Debug.Log("50% de descuento");
+            }
+            if (range == "silver")
+            {
+                Debug.Log("25% de descuento");
+            }
+            if (range == "bronze")
+            {
+                Debug.Log("15 de descuento");
+            }
+        }
+        else 
+        {
+            Debug.Log("No puede ser socio"); 
+        }
+>>>>>>> Stashed changes
 
+        if (month <= 12 && month >= 1)
+        {
+            if (day <= 31)
+            {
+                Debug.Log("El dia existe");
+            }
+
+            if (month == 1)
+            {
+                if (day <= 31 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 2)
+            {
+                if (day <= 28 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 3)
+            {
+                if (day <= 31 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 4)
+            {
+                if (day <= 30 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 5)
+            {
+                if (day <= 31 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 6)
+            {
+                if (day <= 30 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 7)
+            {
+                if (day <= 31 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 8)
+            {
+                if (day <= 31 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 9)
+            {
+                if (day <= 30 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 10)
+            {
+                if (day <= 31 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 11)
+            {
+                if (day <= 30 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+            if (month == 12)
+            {
+                if (day <= 31 && day >= 1)
+                {
+                    Debug.Log("El dia existe");
+                }
+            }
+        }
+
+       {
+            Debug.Log($"{num3} % {num4} = {num3 % num4}");
+       }
+    }
     
 }
 
